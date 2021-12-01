ed61cbb470465e1d62dad7f5c54c90eaff613ab0
jolean lim <joleanflima@outlook.com>
2021-11-30 21:14:58 -0500

    making into a different game

--- wordgame.js
+++ wordgame.js
@@ -9,6 +9,7 @@ function startGame() {
  showTextNode(1)
 }
 
+
 function showTextNode(textNodeIndex) {
  const textNode = textNodes.find(textNode => textNode.id ===
      textNodeIndex)
@@ -45,38 +46,83 @@ function selectOption(option) {
 const textNodes = [
     {
         id: 1,
-        text: "choose your person",
+        text: "choose where to start",
         options: [
             {
-                text: "Jolean",
-                setState: { jolean: true},
+                text: "North America",
+                setState: { northAmerica: true},
                 nextText: 2
             },
             {
-                text: "Madman",
-                setState: { madman: true},
+                text: "South America",
+                setState: { SouthAmerica: true},
+                nextText: 2
+            },
+            {
+                text: "Europe",
+                setState: { Europe: true},
+                nextText: 2
+            },
+            {
+                text: "Africa",
+                setState: { africa: true},
+                nextText: 2
+            },
+            {
+                text: "Asia",
+                setState: { asia: true},
+                nextText: 2
+            },
+            {
+                text: "Australia",
+                setState: { australia: true},
                 nextText: 2
             },
             {
                 id: 2,
-                text: "choose your character",
+                text: "where would you like to travel to?",
                 options: [
                     {
-                        text: "lucian",
-                        requiredState: (currentState) => currentState.jolean,
-                        setState: { jolean: true, lucian: true },
+                        text: "NorthAmerica",
+                        requiredState: (currentState) => currentState.northAmerica,
+                        setState: { northAmerica: true },
                         nextText: 3
                     },
                     {
-                        text: "darius",
-                        requiredState: (currentState) => currentState.madman,
-                        setState: { madman: true, darius: true},
-                        nextText: 3
-                    }
+                        text: "SouthAmerica",
+                        requiredState: (currentState) => currentState.SouthAmerica,
+                        setState: { SouthAmerica: true },
+                        nextText: 4
+                    },
+                    {
+                        text: "Europe",
+                        requiredState: (currentState) => currentState.Europe,
+                        setState: { europe: true },
+                        nextText: 5
+                    },
+                    {
+                        text: "Africa",
+                        requiredState: (currentState) => currentState.africa,
+                        setState: { africa: true },
+                        nextText: 6
+                    },
+                    {
+                        text: "Asia",
+                        requiredState: (currentState) => currentState.asia,
+                        setState: { asia: true },
+                        nextText: 7
+                    },
+                    {
+                        text: "Australia",
+                        requiredState: (currentState) => currentState.australia,
+                        setState: { australia: true },
+                        nextText: 8
+                    },
                 ]
             },
             {
-                id: 3 
+                id: 3,
+
             }
 
 
